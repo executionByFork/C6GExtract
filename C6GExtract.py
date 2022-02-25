a6b08569b158ce9710793e5d7e85a30f358f8f98
executionByFork <20407804+executionByFork@users.noreply.github.com>
2022-02-25 15:20:06 -0600

    Accept multiple credential files Script now loops on a list of supplied credential files and combines them all into one output instead of only accepting one. Additionally, a version.txt file is written to the folder to note the C6GExtract version used for parsing.

--- C6GExtract.py
+++ C6GExtract.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 __author__ = "executionByFork"
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 __license__ = "idc"
 
 import os
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(description='Converts a CyberSixGill formatted 
 parser.add_argument('-v', '--version', action='version', version=__version__)
 parser.add_argument('-o', '--output-dir', dest='out_dir', default='C6G_output',
                     help='output directory to write files')
-parser.add_argument('input_file', type=str,
+parser.add_argument('input_files', type=str, nargs='+',
                     help='CyberSixGill input csv file')
 args = parser.parse_args()
 
@@ -36,24 +36,25 @@ def isPossiblePassword(string):
 
 
 def main():
-  if args.input_file is None:
+  if not args.input_files:
     print("Error: Please specify an input file. See --help")
     exit()
 
-  with open(args.input_file, newline='') as csvfile:
-    dialect = csv.Sniffer().sniff(csvfile.read(1024))
-    csvfile.seek(0)
-    reader = csv.reader(csvfile, dialect)
-    next(reader) # Skip header line
+  emailList = []
+  credList = []
+  for item in args.input_files:
+    with open(item, newline='') as csvfile:
+      dialect = csv.Sniffer().sniff(csvfile.read(1024))
+      csvfile.seek(0)
+      reader = csv.reader(csvfile, dialect)
+      next(reader) # Skip header line
 
-    emailList = []
-    credList = []
-    for row in reader:
-      # CyberSixGill is pretty terrible at properly differentiating passwords from hashes and junk
-      # Instead of relying on the unreliable "Hash" column, we differentiate the two ourselves
-      if isPossiblePassword(row[1]):
-        credList.append([row[0], row[1]])
-      emailList.append(row[0])
+      for row in reader:
+        # CyberSixGill is pretty terrible at properly differentiating passwords from hashes and junk
+        # Instead of relying on the unreliable "Hash" column, we differentiate the two ourselves
+        if isPossiblePassword(row[1]):
+          credList.append([row[0], row[1]])
+        emailList.append(row[0])
 
   if not os.path.exists(args.out_dir):
     os.makedirs(args.out_dir)
@@ -88,13 +89,16 @@ def main():
     for row in emailList:
       writer.writerow([row])
 
-  # Write all emails with at least one password, plus a count of how many passwords were found
+  # Write all emails with at least one password, plus a count of available passwords for that email
   dictCount = dict(sorted(dictCount.items(), reverse=True, key=lambda item: item[1]))
   with open(args.out_dir + '/C6G_metadata.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['NUM_CREDS','EMAIL'])
     for email,count in dictCount.items():
       writer.writerow([count,email])
+
+  with open(args.out_dir + '/version.txt', 'w', newline='') as txtfile:
+    txtfile.write('Parsed with C6GExtract version ' + __version__)
 
 
 main()
