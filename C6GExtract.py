929780fea71080ea564c44ac78706d77075fa956
executionByFork <20407804+executionByFork@users.noreply.github.com>
2022-02-28 12:51:11 -0600

    improve filtering

--- C6GExtract.py
+++ C6GExtract.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 __author__ = "executionByFork"
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __license__ = "idc"
 
 import os
@@ -13,23 +13,38 @@ parser = argparse.ArgumentParser(description='Converts a CyberSixGill formatted 
 parser.add_argument('-v', '--version', action='version', version=__version__)
 parser.add_argument('-o', '--output-dir', dest='out_dir', default='C6G_output',
                     help='output directory to write files')
+parser.add_argument('-u', '--filter-unlikely', dest='filter_unlikely', action='store_true',
+                    help='Remove passwords that are unlikely to be valid. This has a small chance of removing valid passwords and is off by default')
+parser.set_defaults(filter_unlikely=False)
 parser.add_argument('input_files', type=str, nargs='+',
                     help='CyberSixGill input csv file')
 args = parser.parse_args()
 
-
-hexpattern = re.compile('[0-9a-fA-F]+')
-def isPossiblePassword(string):
+digitsonlypattern = re.compile('^\d+$')
+hexpattern = re.compile('^(0x)?[0-9a-fA-F]{32,40}$')
+bcryptpattern = re.compile('^\$[1-9][a,x,y]?\$\d{1,2}\$.+$')
+# Below function is used to filter out common non-password occurances
+# This isn't meant to catch everything, just the most common cases
+def isPossiblePassword(email, string):
   if not string:  # Remove blank lines
     return False
-  if string[0:4] == '$2a$':  # Remove bcrypt hashes
+  if string == 'xxx':  # Remove "xxx" placeholders
     return False
-  if string[0:11] == '\\_\\_SEC\\_\\_':  # Remove unknown SEC hashes
+  if string[0:11] == '\\_\\_SEC\\_\\_' or string[0:7] == '__SEC__':  # Remove unknown SEC hashes
     return False
-  if hexpattern.match(string):
-    if len(string) == 32:  # Remove MD5 / MD4 hashes
+  if bcryptpattern.match(string):  # Remove bcrypt hashes
+    return False
+  if hexpattern.match(string):  # Remove MD5 / MD4 / SHA1 hashes
+    return False
+
+  if args.filter_unlikely:  # Filter out password entries that are unlikely to be valid
+    if len(string) >= 25:  # Remove strings with 25+ characters
       return False
-    if len(string) == 40:  # Remove SHA1 hashes
+    if len(string) < 8:  # Remove strings with fewer than 8 characters
+      return False
+    if email.partition('@')[2] == string:  # Remove strings that match the email domain exactly
+      return False
+    if digitsonlypattern.match(string):  # Remove strings with only digits
       return False
 
   return True  # Otherwise, string is a possible password
@@ -52,7 +67,7 @@ def main():
       for row in reader:
         # CyberSixGill is pretty terrible at properly differentiating passwords from hashes and junk
         # Instead of relying on the unreliable "Hash" column, we differentiate the two ourselves
-        if isPossiblePassword(row[1]):
+        if isPossiblePassword(row[0], row[1]):
           credList.append([row[0], row[1]])
         emailList.append(row[0])
 
